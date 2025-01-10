df5a94c167567e852ca8a68e96a4d22f128dd638
Abhishek Sharma <sharmaabhi10101010@gmail.com>
2025-01-10 09:24:39 +0545

    updated js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // Firebase Configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyA4dgXXg-GO7M0w1gPI7xXF4JlsmcGJBT0",
   authDomain: "beamsmart-7b041.firebaseapp.com",
   databaseURL: "https://beamsmart-7b041-default-rtdb.firebaseio.com",
   projectId: "beamsmart-7b041",
-  storageBucket: "beamsmart-7b041.appspot.com",
+  storageBucket: "beamsmart-7b041.firebasestorage.app",
   messagingSenderId: "591934946040",
   appId: "1:591934946040:web:dac2f85aa2fd3d3fee190d",
   measurementId: "G-28LQCHPEER"
